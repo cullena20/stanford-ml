f3716e0eb2366191b00582f404e57a87dd0c2bf6
Cullen <cullenyanderson@gmail.com>
2021-01-19 22:51:56 -0500

    Completed week 3 excercises

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,9 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+prediction = sigmoid(X * theta)
 
+J = (1 / m) * ((-y' * log(prediction)) - (1 - y)' * log(1 - prediction)) + (lambda / (2 * m)) * sum(theta(2:length(theta), :) .^ 2)
 
+grad = (1 / m) * (X' * (prediction - y)) + (lambda / m) .* theta
 
+grad(1) = (1 / m) * sum(X(1) .* (prediction - y))
 
 
 % =============================================================
